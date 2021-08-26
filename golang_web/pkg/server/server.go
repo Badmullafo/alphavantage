e89384361b4dfdedc09134f9681fee97c0043c2d
alex.wilson <alex.wilson@forgerock.com>
2021-08-26 11:19:31 +0100

    Moved consts from pkg/server/server.go to pkg/request/get.go

--- golang_web/pkg/server/server.go
+++ golang_web/pkg/server/server.go
@@ -7,14 +7,6 @@ import (
 	_ "github.com/Badmullafo/alphavantage/golang_web/pkg/request"
 )
 
-const (
-	layoutISO = "2006-01-02"
-	tz        = "US/Eastern"
-	layoutUS  = "January 2, 2006"
-	urlS      = "https://www.alphavantage.co/query?apikey="
-	rtype     = "&function=TIME_SERIES_DAILY_ADJUSTED&symbol="
-)
-
 func Startserver() {
 
 	fmt.Printf("Starting server at port 8080\n")
